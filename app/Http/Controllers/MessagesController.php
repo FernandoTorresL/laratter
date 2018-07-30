7007d7fb53cb0542bdb2bca3617ecc2f736ad73c
Fernando Torres <fertorresmx@gmail.com>
2018-07-30 01:15:49 -0500

    Archivos video 13b 'Validación y creación de datos en Laravel con Eloquent'

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateMessageRequest;
 use App\Message;
 use Illuminate\Http\Request;
 
@@ -14,16 +15,18 @@ class MessagesController extends Controller
             ]);
     }
 
-    public function create(Request $request)
+    public function create(CreateMessageRequest $request)
     {
-        $this->validate($request, [
-            'message' => ['required', 'max:160']
-            ], [
-                'message.required' => 'Por favor escribe tu mensaje.',
-                'message.max' => 'El mensaje no puede superar los 160 caracteres.'
-                ]);
-
+//        $this->validate($request);
 //        dd($request->all());
-        return 'Llegó!';
+
+        $message = Message::create([
+            'content' => $request->input('message'),
+            'image' => 'http://placeimg.com/600/338/tech?' . mt_rand(0,1000)
+        ]);
+
+//        dd($message);
+
+        return redirect('/messages/'.$message->id);
     }
 }
